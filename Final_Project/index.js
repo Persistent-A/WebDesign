154fbd7b51581de41d4cda2e1a9e8dd7b99314f0
Persistent-A <apurva.mili@gmail.com>
2022-05-05 04:58:28 -0400

    10th draft

--- Final_Project/index.js
+++ Final_Project/index.js
@@ -8,6 +8,19 @@ form.addEventListener("submit", function (e) {
     }, 1);
 });
 
+function openAbout(evt, aboutName){
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for(i=0;i< tabcontent.length; i++){
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for(i = 0; i < tablinks.length; i++){
+        tablinks[i].className = tablinks[i].className.replace("active", "");
+    }
+document.getElementById(aboutName).style.display = "block";
+evt.currentTarget.className += "active";
+}
 
 
 
