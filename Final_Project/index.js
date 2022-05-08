bcc9a780e611e3cac6578e605ffc05b3f7c928c3
Persistent-A <apurva.mili@gmail.com>
2022-05-08 02:41:25 -0400

    learned how to navigate through scroll button

--- Final_Project/index.js
+++ Final_Project/index.js
@@ -1,3 +1,4 @@
+// Form functionality
 
 var submitButton = document.getElementById("submit_form");
 var form = document.getElementById("form");
@@ -7,6 +8,8 @@ form.addEventListener("submit", function (e) {
         submitButton.disabled = true;
     }, 1);
 });
+
+// about us tab functionality
 
 function openAbout(evt, aboutName){
     var i, tabcontent, tablinks;
@@ -21,6 +24,37 @@ function openAbout(evt, aboutName){
 document.getElementById(aboutName).style.display = "block";
 evt.currentTarget.className += "active";
 }
+
+// services button functionality
+
+const servicesContent = ['Welcome to our Services section!', 'We are happy to help you.', 'Please navigate here to select options to avail service on.', 'Congratulations you have successfully navigated our Service section.'];
+const content = document.querySelector('.displayService');
+const serviceButton = document.querySelectorAll('.service-button');
+content.innerHTML = servicesContent[0];
+
+serviceButton.forEach(element => element.addEventListener('click', () => {
+    let currentContentIndex = servicesContent.indexOf(content.innerHTML);
+    if (element.className === 'service-button next') {
+        if (currentContentIndex === (servicesContent.length -1)) {
+            return 0
+        }
+        else {
+            currentContentIndex++;
+            content.innerHTML = servicesContent[currentContentIndex];
+        }
+    }
+    else if(element.className == 'service-button prev') {
+        if (currentContentIndex === 0) {
+            return 0
+        }
+        else {
+            currentContentIndex--;
+            content.innerHTML = servicesContent[currentContentIndex];
+        }
+    }
+}));
+    
+
 
 
 
