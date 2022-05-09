fc3ca97f323b8b3d1646195e89cc66ca363d692b
Persistent-A <apurva.mili@gmail.com>
2022-05-08 22:50:46 -0400

    Final Project Done

--- Final_Project/index.js
+++ Final_Project/index.js
@@ -1,61 +1,70 @@
-// Form functionality
-
-var submitButton = document.getElementById("submit_form");
-var form = document.getElementById("form");
+// FORM - functionality
+    
+const submitButton = document.getElementById("submit_form"); // Use of DOM to get submit button input element
+const form = document.getElementById("form"); // It stores the whole form elemnet to form variable
+    /* on submission of the form, 
+        initialization and execution of a function will be carried out*/
 form.addEventListener("submit", function (e) {
+    /* setTimeout function takes in a callback function and delaytime as an argument in miliseconds
+        it executes the function after delaytime*/
     setTimeout(function() {
         submitButton.value = "Sending...";
-        submitButton.disabled = true;
+        submitButton.disabled = true; // after 1 milisecond the callback function will be executed
     }, 1);
 });
 
-// about us tab functionality
 
-function openAbout(evt, aboutName){
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for(i=0;i< tabcontent.length; i++){
-        tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablinks");
-    for(i = 0; i < tablinks.length; i++){
-        tablinks[i].className = tablinks[i].className.replace("active", "");
-    }
-document.getElementById(aboutName).style.display = "block";
-evt.currentTarget.className += "active";
-}
+// ABOUT US TAB - functionality
 
-// services button functionality
+const tabcontent = document.querySelectorAll(".tabcontent");
+const tablinks = document.querySelectorAll(".tablinks");
+// Iterating over tabs and adding addeventlistener function to make every tab click functional.
+tablinks.forEach(ele => {
+    ele.addEventListener('click', () => {
+        tabcontent.forEach(element => element.style.display = "none"); // Content of all tabs will get invisible.
+        tablinks.forEach(element => element.className = element.className.replace(" active", "")); // Replaces all active className to 'active'
+        document.getElementById(ele.innerHTML).style.display = "block"; // Content of clicked tab will get visible
+        ele.className += " active"; // clicked tab will get class name - 'tablink active'
+    })
+})
+document.getElementById("defaultOpen").click(); 
 
-const servicesContent = ['Welcome to our Services section!', 'We are happy to help you.', 'Please navigate here to select options to avail service on.', 'Congratulations you have successfully navigated our Service section.'];
-const content = document.querySelector('.displayService');
+// Services button - functionality
+
+const servicesContent = ['Welcome to our Services section!', 'We are happy to help you.', 'Please navigate here to select options to avail service on.', 'Congratulations you have successfully navigated our Service section.']; //Storing as an array.
+const content = document.querySelector('.displayService'); 
 const serviceButton = document.querySelectorAll('.service-button');
+// Assigning the content to display.
 content.innerHTML = servicesContent[0];
 
+// Iterating over navigation button.
 serviceButton.forEach(element => element.addEventListener('click', () => {
-    let currentContentIndex = servicesContent.indexOf(content.innerHTML);
+    /* for each click on navigation button 
+        the statements will increase or decrese the currentContentIndex
+        and according to new Index it fetches content from stored array 
+        and displays it to services section
+    */
+    let currentContentIndex = servicesContent.indexOf(content.innerHTML); 
     if (element.className === 'service-button next') {
         if (currentContentIndex === (servicesContent.length -1)) {
             return 0
         }
         else {
+            // increment in index if click on next - button
             currentContentIndex++;
             content.innerHTML = servicesContent[currentContentIndex];
         }
     }
-    else if(element.className == 'service-button prev') {
+    else if(element.className === 'service-button prev') {
         if (currentContentIndex === 0) {
             return 0
         }
         else {
+             // decrement in index if click on previous - button
             currentContentIndex--;
             content.innerHTML = servicesContent[currentContentIndex];
         }
     }
 }));
-    
-
-
-
-
+ 
 
